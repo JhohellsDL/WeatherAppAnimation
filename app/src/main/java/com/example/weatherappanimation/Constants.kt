2377b8cb0aaec9060b7eb6dc78bf4633ae6267cc
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2024-01-27 16:07:03 -0500

    add animations for view init

---
+++ app/src/main/java/com/example/weatherappanimation/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.weatherappanimation
+
+private const val ANIMATION_DURATION = 1000L
--- app/src/main/java/com/example/weatherappanimation/MainActivity.kt
+++ app/src/main/java/com/example/weatherappanimation/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.weatherappanimation
 
 import android.animation.Animator
-import android.content.Intent
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.airbnb.lottie.LottieAnimationView
 import com.example.weatherappanimation.databinding.ActivityMainBinding
+
+private const val ANIMATION_DURATION = 1000L
+private const val ANIMATION_DURATION_TEXT = 700L
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,12 +25,17 @@ class MainActivity : AppCompatActivity() {
         val lottieAnimationView: LottieAnimationView = findViewById(R.id.lottieAnimationView)
         val lottieAnimationView2: LottieAnimationView = binding.buttonNext
 
-        // Puedes controlar la animación según sea necesario
-        // Por ejemplo, para iniciar la animación:
         lottieAnimationView.playAnimation()
 
         binding.buttonNext.setOnClickListener {
             lottieAnimationView2.playAnimation()
+            binding.imageWeather.fadeInAndMoveUpAndScale()
+            binding.cardView.fadeInAndMoveUp()
+            moveThreeObjects(
+                binding.textTitle,
+                binding.textSubtitle,
+                binding.viewButton
+            )
         }
 
         lottieAnimationView2.addAnimatorListener(object : Animator.AnimatorListener {
@@ -41,4 +54,90 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    private fun ImageView.fadeInAndMoveUpAndScale() {
+        val fadeInAnimator = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+
+        val scaleAnimatorX = ObjectAnimator.ofFloat(this, "scaleX", 1f, 1.3f)
+        scaleAnimatorX.duration = ANIMATION_DURATION
+
+        val scaleAnimatorY = ObjectAnimator.ofFloat(this, "scaleY", 1f, 1.3f)
+        scaleAnimatorY.duration = ANIMATION_DURATION
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(fadeInAnimator, scaleAnimatorX, scaleAnimatorY)
+        animatorSet.start()
+    }
+
+    private fun CardView.fadeInAndMoveUp() {
+        val fadeInAnimator = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+        val translateAnimator = ObjectAnimator.ofFloat(this, "translationY", 100f, -20f)
+        translateAnimator.duration = ANIMATION_DURATION
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(fadeInAnimator, translateAnimator)
+        animatorSet.start()
+    }
+
+    private fun moveThreeObjects(textView1: TextView, textView2: TextView, view: View) {
+        textView1.alpha = 0f
+        textView2.alpha = 0f
+        view.alpha = 0f
+
+        val fadeInAnimator1 = ObjectAnimator.ofFloat(textView1, "alpha", 0f, 1f)
+        fadeInAnimator1.duration = ANIMATION_DURATION_TEXT
+        val fadeInAnimator2 = ObjectAnimator.ofFloat(textView2, "alpha", 0f, 1f)
+        fadeInAnimator2.duration = ANIMATION_DURATION_TEXT
+        val fadeInAnimator3 = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f)
+        fadeInAnimator3.duration = ANIMATION_DURATION_TEXT
+
+        val animator1 = ObjectAnimator.ofFloat(textView1, "translationY", 0f, 10f)
+        animator1.duration = ANIMATION_DURATION_TEXT
+        val animator2 = ObjectAnimator.ofFloat(textView2, "translationY", 0f, 10f)
+        animator2.duration = ANIMATION_DURATION_TEXT
+
+        val scaleAnimatorX = ObjectAnimator.ofFloat(view, "scaleX", 1f, 1.2f)
+        scaleAnimatorX.duration = ANIMATION_DURATION_TEXT
+        val scaleAnimatorY = ObjectAnimator.ofFloat(view, "scaleY", 1f, 1.2f)
+        scaleAnimatorY.duration = ANIMATION_DURATION_TEXT
+
+        animator1.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                animator2.start()
+                fadeInAnimator2.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+        animator2.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                scaleAnimatorX.start()
+                scaleAnimatorY.start()
+                fadeInAnimator3.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+
+        fadeInAnimator1.start()
+        animator1.start()
+    }
+
 }
