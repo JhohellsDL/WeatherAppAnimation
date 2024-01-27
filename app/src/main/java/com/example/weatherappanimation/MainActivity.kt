c2ab7d05c2e5266eff3cd24d135f58cd229e33a3
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2024-01-27 15:33:24 -0500

    add animation and view ok

--- app/src/main/java/com/example/weatherappanimation/MainActivity.kt
+++ app/src/main/java/com/example/weatherappanimation/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.weatherappanimation
 
+import android.animation.Animator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
 import com.example.weatherappanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val lottieAnimationView: LottieAnimationView = findViewById(R.id.lottieAnimationView)
+        val lottieAnimationView2: LottieAnimationView = binding.buttonNext
+
+        // Puedes controlar la animación según sea necesario
+        // Por ejemplo, para iniciar la animación:
+        lottieAnimationView.playAnimation()
+
+        binding.buttonNext.setOnClickListener {
+            lottieAnimationView2.playAnimation()
+        }
+
+        lottieAnimationView2.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+
+            }
+        })
     }
 }
